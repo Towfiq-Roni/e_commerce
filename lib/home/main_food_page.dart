7639e07cf46d0aef94b5169c743a2ba47da58622
Towfiq-Roni <towfiqroni@gmail.com>
2022-09-28 00:21:35 +0600

    done till sliding bar (video 50:50)

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,58 @@
+import 'package:e_commerce/home/food_page_body.dart';
+import 'package:e_commerce/utils/colors.dart';
+import 'package:e_commerce/widgets/big_text.dart';
+import 'package:e_commerce/widgets/small_text.dart';
+import 'package:flutter/material.dart';
+
+class MainFoodPage extends StatefulWidget {
+  const MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 30, bottom: 20),
+              padding: EdgeInsets.only(left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      BigText(text: "Bangladesh", color: AppColors.mainColor,),
+                      Row(
+                        children: [
+                          SmallText(text: "Dhaka", color: Colors.black54,),
+                          Icon(Icons.arrow_drop_down_rounded)
+                        ],
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(Icons.search, color: Colors.white,),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          FoodPageBody(),
+        ],
+      ),
+    );
+  }
+}
